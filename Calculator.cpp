70df080806356c073ccde3dd740d236f5ed9d76d
saniyachatterjee <saniyaa0103@gmail.com>
2024-08-11 10:15:58 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+int num1, num2, num3, sum=0;
+
+cout<<"enter first number-";
+
+cin>>num1;
+
+cout<<"enter second number";
+
+cin>>num2;
+
+sum=num1+num2;
+
+cout<<"sum of given numbers="<<sum;
+
+return 0;
+
+}
---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+//Srihari Nair
+//PRN 23070123131
+//Exp 1
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello World";
+
+    return 0;
+}
